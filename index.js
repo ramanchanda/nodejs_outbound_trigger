fbd1a9c8b464161e01ff6bce9cb208ec8fe2ec35
Raman Chanda <44603777+ramanchanda@users.noreply.github.com>
2023-07-14 16:46:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_USER_H_URL,
+  connectionString: process.env.DATABASE_USER_URL,
   ssl: {
     rejectUnauthorized: false
   }
